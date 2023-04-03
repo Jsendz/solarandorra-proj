240c946f40542b3827c819bd3b78acfa67576cd7
Jordi Tapies <jorditapies@iMac-de-Jordi.local>
2023-04-03 19:28:01 +0200

    solarandorra-proj

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@
 
 import { Navbar }  from './components/Navbar';
 import { Hero } from './components/Hero';
-import { Instalacion } from './components/Instalacion';
-import { Chackra } from './components/Chackra';
-import { Info } from './components/Info';
+
 import { Contacte } from './components/Contacte';
 
 
@@ -15,9 +13,7 @@ function App() {
     <div>
       <Navbar />
       <Hero />
-      <Instalacion />
-      <Chackra />
-      <Info />
+      
       <Contacte />
       
      
